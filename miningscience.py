028f6945c35d6e16091102337de90b3eaa943715
LeoPRhon <leonardo.proano@est.ikiam.edu.ec>
2022-02-21 09:27:38 -0500

    Leonardo Proaño ha realizado un avance en el ejercicio 1
    Se define la función de download_pubmed para tener un dataframe con los ID de los artículos con una keyword específica

--- miningscience.py
+++ miningscience.py
@@ -1,6 +1,14 @@
-#Vamos a crear la función download_pubmed para poder descargar un dataframe de Pubmed
+#Vamos a crear la funciones download_pubmed y mining_pubs para poder descargar un dataframe de Pubmed
 from Bio import Entrez 
 from Bio import SeqIO
 import pandas as pd
 import re,csv,itertools
 import numpy as np
+
+#Se define la función de download_pubmed para tener un dataframe con los ID de los artículos con una keyword específica
+def download_pubmed(keyword):
+    """"La funcion download_pubmed está encargada de recuperar la ID de los artículos científicos que esten relacionados con la palabra clave que se envió en la base de datos Pubmed """
+    Entrez.email = 'leonardo.proano@est.ikiam.edu.ec'
+    handle = Entrez.esearch(db='pubmed',retmax=10**5 ,retmode='xml',term=keyword)
+    results = Entrez.read(handle)
+    return results
